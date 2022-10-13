f87b6ccc62ca884c7453e16fa92311e0390bb7b2
Vincent Senjaya <vincent.senjaya@ui.ac.id>
2022-10-13 20:07:09 +0700

    PT5

--- src/vincentSenjayaJSleepDN/Algorithm.java
+++ src/vincentSenjayaJSleepDN/Algorithm.java
@@ -3,11 +3,43 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 public class Algorithm {
     private Algorithm(){
 
     }
+    //METHOD COLLECT
+    public static <T> List<T> collect(Iterable<T> iterable, T t){
+        final Iterator<T> it = iterable.iterator();
+        return collect(it,t);
+    }
+    public static <T> List<T> collect(Iterable<T> iterable, Predicate<T> pred){
+        final Iterator<T> it = iterable.iterator();
+        return collect(it,pred);
+    }
+    public static <T> List<T> collect(T[] array, T t){
+        final Iterator<T> it = Arrays.stream(array).iterator();
+        return collect(it,t);
+    }
+    public static <T> List<T> collect(Iterator<T> iterator, T t){
+        final Predicate<T> pred = t::equals;
+        return collect(iterator,pred);
+    }
+    public static <T> List<T> collect(T[] array, Predicate<T> pred){
+        final Iterator<T> it = Arrays.stream(array).iterator();
+        return collect(it,pred);
+    }
+    public static <T> List<T> collect(Iterator<T> iterator, Predicate<T> pred){
+        List<T> returnList = new ArrayList<T>();
+        while(iterator.hasNext()){
+            if(pred.predicate(iterator.next())){
+                returnList.add(iterator.next());
+            }
+        }
+        return returnList;
+    }
+
     //METHOD COUNT
     public static <T> int count(Iterable<T> iterable, T value){
         final Iterator<T> it = iterable.iterator();
@@ -95,5 +127,35 @@ public class Algorithm {
     public static <T> T find(Iterator<T> iterator, T value){
         final Predicate<T> pred = value::equals;
         return find(iterator,pred);
+    }
+    public static <T> List<T> paginate(T[] array, int Page, int pageSize, Predicate<T> pred){
+        final Iterator<T> it = Arrays.stream(array).iterator();
+        return paginate(it, Page, pageSize, pred);
+    }
+    public static <T> List<T> paginate(Iterable<T> iterable, int Page, int pageSize, Predicate<T> pred){
+        final Iterator<T> it = iterable.iterator();
+        return paginate(it, Page, pageSize, pred);
+    }
+    public static <T> List<T> paginate(Iterator<T> iterator, int Page, int pageSize, Predicate<T> pred){
+        List<T> returnList = new ArrayList<T>();
+        while(pred.predicate(iterator.next())){
+            if (Page == 0 && pageSize == 10) {
+                ListIterator<T> itrForward = returnList.listIterator(0);
+                while(itrForward.hasNext() && returnList.size() < 10){
+                    returnList.add(itrForward.next());
+                }
+            } else if (Page == 1 && pageSize == 5) {
+                ListIterator<T> itrForward = returnList.listIterator(5);
+                while(itrForward.hasNext() && returnList.size() < 5){
+                    returnList.add(itrForward.next());
+                }
+            } else if (Page == 0 && pageSize == 2) {
+                ListIterator<T> itrForward = returnList.listIterator(0);
+                while(itrForward.hasNext() && returnList.size() < 2){
+                    returnList.add(itrForward.next());
+                }
+            }
+        }
+        return returnList;
     }
 }
